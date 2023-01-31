98c10b89042e4af94655cde021cc5602774e735f
Abdur740 <123439024+Abdur740@users.noreply.github.com>
2023-01-31 14:37:38 +0530

    Final code

--- src/main/java/assignment/App.java
+++ src/main/java/assignment/App.java
@@ -23,7 +23,6 @@ class Getinput implements Cloneable{
     boolean check(int x1, int y1){
         return x==x1 && y==y1;
     }
-    @Override
     public Object clone()throws CloneNotSupportedException{
         return super.clone();
     }
@@ -44,11 +43,17 @@ class App{
             boolean result1 = obj1.check(x1,y1);
             String displayresult = String.valueOf(result1);
             LOGGER.info(displayresult);
-            LOGGER.info("Original Coordinate x: {}",obj1.x);
-            LOGGER.info("Original Coordinate y: {}",obj1.y);
+            String originalcoorx = String.valueOf(obj1.x);
+            String print = "Original Coordinate: "+originalcoorx;
+            LOGGER.info(print);
+            String originalcoory = String.valueOf(obj1.y);
+            String print1 = "Original Coordinate: "+originalcoory;
+            LOGGER.info(print1);
             objclone.x=x1;
-            LOGGER.info("Clonned Coordinate x2: {}",objclone.x);
+            String print2 = "Clonned Coordinate x2: "+objclone.x; 
+            LOGGER.info(print2);
             objclone.y=y1;
-            LOGGER.info("Clonned Coordinate y2: {}",objclone.y);       
+            String print3 = "Clonned Coordinate y2: "+objclone.y;
+            LOGGER.info(print3);       
     }
 }
